38b472de115e769e1bc0cb76cfb64eeec137f9be
Hayin Ananta <72958200+hayinananta@users.noreply.github.com>
2020-11-06 19:25:10 +0700

    Add files via upload

---
+++ src/if1/pkg10119005/latihan39/objectorientednilaiterbesarterkecil/IF110119005Latihan39ObjectOrientedNilaiTerbesarTerkecil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119005.latihan39.objectorientednilaiterbesarterkecil;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Program ini memilah mana nilai Terbesar dan mana nilai
+ *                     Terkecil
+ */
+public class IF110119005Latihan39ObjectOrientedNilaiTerbesarTerkecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         NilaiTerbesarTerkecil nilaiMahasiswa = new NilaiTerbesarTerkecil();
+         nilaiMahasiswa.tampilNilaiMahasiswa();
+    }   
+}
---
+++ src/if1/pkg10119005/latihan39/objectorientednilaiterbesarterkecil/NilaiTerbesarTerkecil.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119005.latihan39.objectorientednilaiterbesarterkecil;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Program ini memilah mana nilai Terbesar dan mana nilai
+ *                     Terkecil
+ */
+public class NilaiTerbesarTerkecil {
+    private static Scanner scanner = new Scanner(System.in);
+    private static String namaPetugas;
+    private static int banyakNilaiMahasiswa;
+    private static int nilaiMahasiswa[];
+    private static int nilaiTerkecil = 0;
+    private static int nilaiTerbesar = 0;
+    
+    private static String inputNamaMahasiswa() {
+        System.out.println("=====Program Nilai Terbesar dan Terkecil Mahasiswa=====");
+        System.out.print("Masukkan Nama Petugas : ");
+        namaPetugas = scanner.nextLine();
+        return namaPetugas;
+    }
+    
+    private static int inputNilaiMahasiswa() {
+        inputNamaMahasiswa();
+        System.out.print("Masukkan Banyaknya Jumlah Nilai Mahasiswa : ");
+        banyakNilaiMahasiswa = scanner.nextInt();
+
+        nilaiMahasiswa = new int[banyakNilaiMahasiswa];
+
+        for (int i = 0; i < banyakNilaiMahasiswa; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa Ke-" + (i + 1) + " = ");
+            nilaiMahasiswa[i] = scanner.nextInt();
+        }
+        return 0;
+    }
+    
+        private static void cekNilaiTerbesarTerkecil() {
+        inputNilaiMahasiswa();
+        System.out.println("\n=====Hasil Nilai Mahasiswa=====");
+        nilaiTerkecil = nilaiMahasiswa[0];
+        for (int i = 0; i < banyakNilaiMahasiswa; i++) {
+            System.out.println("Nilai Mahasiswa Ke-" + (i + 1) + " = " + nilaiMahasiswa[i]);
+
+            if (nilaiMahasiswa[i] < nilaiTerkecil) {
+                nilaiTerkecil = nilaiMahasiswa[i];
+            }
+
+            if (nilaiMahasiswa[i] > nilaiTerbesar) {
+                nilaiTerbesar = nilaiMahasiswa[i];
+            }
+        }
+    }
+            
+    public static void tampilNilaiMahasiswa() {
+        cekNilaiTerbesarTerkecil();
+        System.out.println();
+        System.out.println("Nilai Terbesar adalah " + nilaiTerbesar);
+        System.out.println("Nilai Terkecil adalah " + nilaiTerkecil);
+
+        System.out.println("\nPetugas : " + namaPetugas);
+    }
+}
+    
